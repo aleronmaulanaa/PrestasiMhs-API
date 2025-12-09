c7055312e56c20b01ea6dcb15807f555caeea29a
aleronmaulanaa <aleronmaulanafirjatullah@gmail.com>
2025-12-09 23:34:31 +0700

    feat: update model MongoDB AchievementDetails (penyesuaian struktur dan ketentuan data)

--- app/models/achievement_model.go
+++ app/models/achievement_model.go
@@ -94,23 +94,62 @@ type AchievementMongo struct {
 }
 
 // AchievementDetails menangani field dinamis
+// type AchievementDetails struct {
+// 	// Competition
+// 	CompetitionName  string `bson:"competitionName,omitempty" json:"competition_name,omitempty"`
+// 	CompetitionLevel string `bson:"competitionLevel,omitempty" json:"competition_level,omitempty"`
+// 	Rank             int    `bson:"rank,omitempty" json:"rank,omitempty"`
+// 	MedalType        string `bson:"medalType,omitempty" json:"medal_type,omitempty"`
+	
+// 	// Organization
+// 	OrganizationName string    `bson:"organizationName,omitempty" json:"organization_name,omitempty"`
+// 	Position         string    `bson:"position,omitempty" json:"position,omitempty"`
+// 	StartDate        time.Time `bson:"startDate,omitempty" json:"start_date,omitempty"`
+// 	EndDate          time.Time `bson:"endDate,omitempty" json:"end_date,omitempty"`
+
+// 	// General
+// 	EventDate time.Time `bson:"eventDate,omitempty" json:"event_date,omitempty"`
+// 	Location  string    `bson:"location,omitempty" json:"location,omitempty"`
+// 	Organizer string    `bson:"organizer,omitempty" json:"organizer,omitempty"`
+// }
+// app/models/achievement_model.go
+
+// AchievementDetails menangani field dinamis
 type AchievementDetails struct {
-	// Competition
+	// --- Competition ---
 	CompetitionName  string `bson:"competitionName,omitempty" json:"competition_name,omitempty"`
 	CompetitionLevel string `bson:"competitionLevel,omitempty" json:"competition_level,omitempty"`
 	Rank             int    `bson:"rank,omitempty" json:"rank,omitempty"`
 	MedalType        string `bson:"medalType,omitempty" json:"medal_type,omitempty"`
-	
-	// Organization
+
+	// --- Organization ---
 	OrganizationName string    `bson:"organizationName,omitempty" json:"organization_name,omitempty"`
 	Position         string    `bson:"position,omitempty" json:"position,omitempty"`
 	StartDate        time.Time `bson:"startDate,omitempty" json:"start_date,omitempty"`
 	EndDate          time.Time `bson:"endDate,omitempty" json:"end_date,omitempty"`
 
-	// General
+	// --- Publication (NEW - Sesuai SRS) ---
+	PublicationType  string   `bson:"publicationType,omitempty" json:"publication_type,omitempty"`
+	PublicationTitle string   `bson:"publicationTitle,omitempty" json:"publication_title,omitempty"`
+	Authors          []string `bson:"authors,omitempty" json:"authors,omitempty"`
+	Publisher        string   `bson:"publisher,omitempty" json:"publisher,omitempty"`
+	ISSN             string   `bson:"issn,omitempty" json:"issn,omitempty"`
+
+	// --- Certification (NEW - Sesuai SRS) ---
+	CertificationName   string    `bson:"certificationName,omitempty" json:"certification_name,omitempty"`
+	IssuedBy            string    `bson:"issuedBy,omitempty" json:"issued_by,omitempty"`
+	CertificationNumber string    `bson:"certificationNumber,omitempty" json:"certification_number,omitempty"`
+	ValidUntil          time.Time `bson:"validUntil,omitempty" json:"valid_until,omitempty"`
+
+	// --- General ---
 	EventDate time.Time `bson:"eventDate,omitempty" json:"event_date,omitempty"`
 	Location  string    `bson:"location,omitempty" json:"location,omitempty"`
 	Organizer string    `bson:"organizer,omitempty" json:"organizer,omitempty"`
+	Score     float64   `bson:"score,omitempty" json:"score,omitempty"` // NEW
+
+	// --- Dynamic Extension (NEW - Sesuai SRS) ---
+	// Menampung field lain yang tidak terdefinisi (SRS: customFields?: Object)
+	CustomFields map[string]interface{} `bson:"customFields,omitempty" json:"custom_fields,omitempty"`
 }
 
 type Attachment struct {
